5b819e16a9aefa5e01e4d10041192106875c32e5
rajeevc <rajeev.chandrasekaran@appliedcognition.com>
2021-09-02 21:36:22 -0700

    sensorkit - first version to successfully capture keyboard metrics

---
+++ longevityApp.swift
@@ -0,0 +1,18 @@
+//
+//  longevityApp.swift
+//  longevity
+//
+//  Created by Rajeev Chandrasekaran on 6/2/21.
+//
+
+import SwiftUI
+
+@main
+struct longevityApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    };
+}
